e33ffb3f0bfdcae2c491007297547f55b23fd914
Marcio Rodriguez <marciorodriguez91@gmail.com>
2016-06-08 16:56:11 -0300

    Update gitApi.sh

--- gitApi.sh
+++ gitApi.sh
@@ -1,1 +1,1 @@
-curl "https://api.github.com/repos/$TRAVIS_REPO_SLUG/statuses/$TRAVIS_COMMIT?access_token=6165f32f01e470f327575731c4d5af2dd644e0a9" -H "Content-Type: application/json" -X POST -d "{'state': 'pending', 'description': 'Monkop', 'target_url': 'http://console.monkop.com'}"
+curl "https://api.github.com/repos/$TRAVIS_REPO_SLUG/statuses/$TRAVIS_COMMIT?access_token=5e638850f16674431e60d7d9ece009ec5239168a" -H "Content-Type: application/json" -X POST -d "{'state': 'pending', 'description': 'Monkop', 'target_url': 'http://console.monkop.com'}"
